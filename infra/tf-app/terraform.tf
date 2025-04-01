a52afe9cc8e889c86f54139d2cf05c326d09d291
Savas Erturk <34849558+savaserturk@users.noreply.github.com>
2025-03-31 20:34:05 -0400

    Feature/lab12 backend (#7)
    * feat: complete 2-terraform-backend.md - setup remote state storage for Lab 12
    
    * Add federated credentials and Terraform output updates for GitHub Actions
    
    * Enable OIDC authentication in Terraform backend and provider

--- infra/tf-app/terraform.tf
+++ infra/tf-app/terraform.tf
@@ -1,11 +1,5 @@
 terraform {
-  backend "azurerm" {
-    resource_group_name  = "ertu0002-githubactions-rg"
-    storage_account_name = "ertu0002githubactions"
-    container_name       = "tfstate"
-    key                  = "prod.app.tfstate"
-  }
-
+  required_version = "~> 1.5"
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
@@ -14,6 +8,15 @@ terraform {
   }
 }
 
+ backend "azurerm" {
+    resource_group_name  = "ertu0002-githubactions-rg"
+    storage_account_name = "ertu0002githubactions"
+    container_name       = "tfstate"
+    key                  = "prod.app.tfstate"
+    use_oidc             = true
+  }
+
 provider "azurerm" {
   features {}
+  use_oidc = true
 }
