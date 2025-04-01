2496ca19c8bf93a19994013c571d8fc5ae0fdffc
buru0003 <buru0003@algonquinlive.com>
2025-03-31 22:03:27 -0400

    Feat: Configured Integration tests

--- infra/tf-app/terraform.tf
+++ infra/tf-app/terraform.tf
@@ -1,37 +1,22 @@
 terraform {
-<<<<<<< HEAD
-  backend "azurerm" {
-    resource_group_name  = "ertu0002-githubactions-rg"
-    storage_account_name = "ertu0002githubactions"
-    container_name       = "tfstate"
-    key                  = "prod.app.tfstate"
-  }
-
-=======
   required_version = "~> 1.5"
->>>>>>> origin/main
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
       version = "~> 3.0"
     }
   }
-}
-
-<<<<<<< HEAD
-provider "azurerm" {
-  features {}
-=======
- backend "azurerm" {
+   backend "azurerm" {
     resource_group_name  = "ertu0002-githubactions-rg"
     storage_account_name = "ertu0002githubactions"
     container_name       = "tfstate"
     key                  = "prod.app.tfstate"
     use_oidc             = true
   }
+}
+
 
 provider "azurerm" {
   features {}
   use_oidc = true
->>>>>>> origin/main
 }
