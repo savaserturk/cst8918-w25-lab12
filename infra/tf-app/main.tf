fd908f16b5716b5d106b32f18e4e1110c1294e8b
buru0003 <buru0003@algonquinlive.com>
2025-04-01 02:59:01 -0400

    Feat: Adding Vnet and Subnet config

--- infra/tf-app/main.tf
+++ infra/tf-app/main.tf
@@ -2,3 +2,21 @@ resource "azurerm_resource_group" "app_rg" {
   name     = "ertu0002-a12-rg"
   location = "Canada Central"
 }
+
+
+# Define the virtual network
+resource "azurerm_virtual_network" "vnet" {
+  name                = "ertu0002-a12-A12Vnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.app_rg.location
+  resource_group_name = azurerm_resource_group.app_rg.name
+}
+
+
+# Define the subnet
+resource "azurerm_subnet" "webserver" {
+  name                 = "ertu0002-a12-A12Subnet"
+  resource_group_name  = azurerm_resource_group.app_rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
