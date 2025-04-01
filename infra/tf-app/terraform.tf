09639c956fc109cb4602e87ecd5921994d28fd1f
Seyfullah Burul <75156011+sburul@users.noreply.github.com>
2025-04-01 00:32:57 -0400

    Feature/lab12 GitHub terraform integration (#12)
    * Feat:Github actions terraform-integration
    
    * Fix: Terraform file format fixed

--- infra/tf-app/terraform.tf
+++ infra/tf-app/terraform.tf
@@ -1,28 +1,13 @@
 terraform {
-<<<<<<< HEAD
-  backend "azurerm" {
-    resource_group_name  = "ertu0002-githubactions-rg"
-    storage_account_name = "ertu0002githubactions"
-    container_name       = "tfstate"
-    key                  = "prod.app.tfstate"
-  }
-
-=======
   required_version = "~> 1.5"
->>>>>>> origin/main
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
       version = "~> 3.0"
     }
   }
-}
 
-<<<<<<< HEAD
-provider "azurerm" {
-  features {}
-=======
- backend "azurerm" {
+  backend "azurerm" {
     resource_group_name  = "ertu0002-githubactions-rg"
     storage_account_name = "ertu0002githubactions"
     container_name       = "tfstate"
@@ -30,8 +15,9 @@ provider "azurerm" {
     use_oidc             = true
   }
 
+}
+
 provider "azurerm" {
   features {}
   use_oidc = true
->>>>>>> origin/main
 }
